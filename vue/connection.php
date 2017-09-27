dbe9d0b529f836783f1d3c7da2d84cb2d398f7b1
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-09-27 14:25:31 +0200

    connection & logging vue

---
+++ vue/connection.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container" id="inscription">
+    <h1>Se connecter :</h1>
+    <form class="" action="../controle/connexion_post.php" method="post">
+      <input type="text" name="pseudo" value="" placeholder="mail">
+      <input type="password" name="password" value="" placeholder="password">
+      <p><?php if(isset($_SESSION['erreur']["connexion"])) {
+          echo 'erreur de connexion';
+        }?></p>
+      <input type="submit" name="" value="connexion">
+    </form>
+  </div>
+
+    <div class="container" id="inscription">
+      <h1>Créer un compte :</h1>
+      <form class="" action="../controle/enregistrement.php" method="post">
+        <input type="submit" name="" value="inscription">    </div>
+      </form>
+  </body>
+</html>
