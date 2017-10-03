7492d340edda2f0d20719cfb6fb49dc9b1ddae47
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-10-03 10:51:57 +0200

    end comment

--- controle/end_task.php
+++ controle/end_task.php
@@ -1,6 +1,8 @@
 <?php
 include_once '../modele/data.php';
 include('header.php');
+
+// $_POST['endtask'] = id_step from descriptive.php 
 send_end_task($_POST['endtask']);
 $variable = get_user_id();
 include '../vue/index.php';
--- controle/logging_post.php
+++ controle/logging_post.php
@@ -1,5 +1,5 @@
 <?php
- 
+// take pseudo,password, name .. from logging.php
  $pseudo=strtolower($_POST['pseudo']);
  $pass=$_POST['password'];
  $password1=$_POST['password1'];
@@ -7,27 +7,35 @@
  $surname=$_POST['surname'];
  $age=$_POST['age'];
  $mail=$_POST['mail'];
+
+ // crypting password
  $password=sha1($pass);
  $password2=sha1($password1);
  include('../modele/data.php');
+
+ // compare connection
  $donnees=compare_pseudo($pseudo);
+
 
 if (isset($_POST['mail']))
 {
-    $_POST['mail'] = htmlspecialchars($_POST['mail']); // On rend inoffensives les balises HTML que le visiteur a pu rentrer
+    // security
+    $_POST['mail'] = htmlspecialchars($_POST['mail']);
+
 
    if ($pseudo==$donnees['pseudo'] or !preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail']))
     {
+      // connection pseudo
       if ($pseudo==$donnees['pseudo'])
       {
         $_SESSION['erreur']["pseudo"]="erreur";
       }
-
+      // connection password
      if ($password!==$password1)
       {
         $_SESSION['erreur']["password"]="erreur";
       }
-
+      // regex for mail
      if (!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail']))
       {
       $_SESSION['erreur']["email"]="erreur";
@@ -36,6 +44,7 @@ if (isset($_POST['mail']))
         include('logging.php');
     }
     else {
+      // logging
      send_register($pseudo, $password, $mail, $name, $surname, $age);
      include('connection.php');
     }
